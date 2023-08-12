90b17c04f3e69447abe2495d1e085970549de2f6
Arise Damilare <arisedamilare5@gmail.com>
2023-08-12 23:55:05 +0100

    13th aug

--- fixful/settings.py
+++ fixful/settings.py
@@ -82,14 +82,11 @@ WSGI_APPLICATION = "fixful.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": 'fixful_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS':  {
-            'autocommit':True,
-        }
+        "NAME": 'DamilareArise$fixful_db',
+        'USER': 'DamilareArise',
+        'PASSWORD': 'Abidemi1#',
+        'HOST': 'DamilareArise.mysql.pythonanywhere-services.com',
+       
     }
 
 }
